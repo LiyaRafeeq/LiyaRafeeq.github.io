0f2d8d29b7deef46a0cb52abbd0d1c56bea6922d
LiyaRafeeq <lr3258@nyu.edu>
2024-12-10 12:04:06 +0400

    personalportfolio

---
+++ personalportfolio/who_script.js
@@ -0,0 +1,9 @@
+function showDescription(boxId) {
+    const box = document.getElementById('box' + boxId);
+    const description = document.getElementById('desc' + boxId);
+  
+    // Toggle the description visibility
+    box.classList.toggle('active');
+    description.classList.toggle('active');
+  }
+  
